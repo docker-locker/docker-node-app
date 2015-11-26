6128ad6e6e2f94afd50205e41c35efd2a0814bd2
Renjith G R <grenjithr@gmail.com>
2015-11-26 23:56:10 +0530

    Adding hostname to the message returned to the user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var os = require("os");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! from ' + os.hostname());
 });
 
 var server = app.listen(3000, function () {
